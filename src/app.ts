615dde410ba71e152b0084b79b7a729a6a804b7b
Joao Filho <joaofilhomil@gmail.com>
2024-04-09 10:17:32 -0300

    adding helmet library for API security

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,14 @@
 import "express-async-errors" // para async quando for trabalhar com BD => npm install express-async-errors
+
 import express, { json } from "express";
 import { taskRouter } from "./routes/task.routes";
 import { HandleErrors } from "./errors/handleErrors";
+import helmet from "helmet"; // Extremamente necessário para segurança para protege a aplicação de diversas vulnerabilidades
 
 export const app = express();
 
+app.use(helmet());
 app.use(json());
 app.use("/tasks", taskRouter);
 // Aplicando o handleErrors.ts
 app.use(HandleErrors.execute);
-
-// Ler múltiplas tasks e fazer uma busca pelo nome da task
-// Ler uma única task
-// Criar uma task
-// Atualizar uma task
-// Excluir uma task
